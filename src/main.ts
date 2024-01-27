61988dcaadb551ffb6ec40f73d99d1f09f3f4509
y-l-mochizuki <mczk9402@gmail.com>
2024-01-27 22:22:06 +0900

    add: section7

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,37 @@
-import express from 'express'
+import express from "express";
+import morgan from "morgan";
+import "express-async-errors";
 
-const PORT = 3000
+const PORT = 3000;
 
-const app = express()
+const app = express();
 
-app.get('/api/hello', async (req, res) => {
+app.use(morgan("dev"));
+
+app.get("/api/hello", async (req, res) => {
   res.json({
-    message: 'Hello Express'
-  })
-})
+    message: "Hello Express",
+  });
+});
+
+app.get("/api/error", async (req, res) => {
+  throw new Error("Error endpoint");
+});
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
-  console.log(`Reversi application started: http://localhost:${PORT}`)
-})
+  console.log(`Reversi application started: http://localhost:${PORT}`);
+});
+
+function errorHandler(
+  error: any,
+  _req: express.Request,
+  res: express.Response,
+  _next: express.NextFunction,
+) {
+  console.error("Unexpected error occurred", error);
+  res.status(500).send({
+    message: "Unexpected error occurred",
+  });
+}
